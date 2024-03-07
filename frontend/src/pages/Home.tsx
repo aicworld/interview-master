91d5c10f214fb6de709ef46d533d084858ac1396
ahao-o <zhihaohe0906@gmail.com>
2024-03-07 23:46:53 +0800

    整理格式

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -12,9 +12,8 @@ import { v4 as uuidv4 } from 'uuid';
 export default function Home() {
   const { scene, level } = useParams<{ scene: string; level: string }>();
   
-  //初始化模拟面试场景，只执行一次
+  //初始化模拟面试场景
   if (scene !== undefined && level !== undefined) {
-    //初始化模拟面试场景，只执行一次
     const { sendMessage } = useChatInteract();
     const placeholder: IAttachment[] = [];
     const initMessage: IStep = {
@@ -26,12 +25,10 @@ export default function Home() {
       output: '',
       createdAt: new Date().toISOString()
     };
-    // 执行发送消息的逻辑
     sendMessage(initMessage, placeholder);
   }
 
-  
-  
+
   return (
     <Page>
       {/* 参数后面补充感叹号是一种ts的类型断言，强调元素类型 */}
